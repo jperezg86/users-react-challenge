952de7be0b2fe58b34db36dd1c18af51f3618116
jperezg86 <jperezg86@gmail.com>
2021-08-18 13:59:19 -0500

    Users fetched

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"; 
 import {
   Button,
   Container,
@@ -5,70 +6,84 @@ import {
   Title,
   UserImage,
   UserInfo,
-} from './components';
+} from "./components";
+
+import { reqResApi } from "./api/reqRes";
+import { current } from "immer";
 
 const mockData = {
   id: 1,
-  email: 'george.bluth@reqres.in',
-  first_name: 'George',
-  last_name: 'Bluth',
-  avatar: 'https://reqres.in/img/faces/1-image.jpg',
+  email: "george.bluth@reqres.in",
+  first_name: "George",
+  last_name: "Bluth",
+  avatar: "https://reqres.in/img/faces/1-image.jpg",
 };
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(Number.MAX_SAFE_INTEGER);
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const fetchUsers = async () => {
+      const {data} = await reqResApi.get('users?per_page=4',{
+        params: {
+          page : currentPage
+        }
+      });
+      if(data.data.length > 0) {
+        setUsers(data.data);
+        setTotalPages(data.total_pages);
+      }else {
+        alert("No more users"); 
+      }
+  };
+
   return (
     <>
       <Container>
         <Title>Our Team</Title>
-        <Grid>
-          <div className='flex-column items-center'>
-            <UserImage url={mockData.avatar} alt={mockData.first_name} />
-            <UserInfo
-              fullName={`${mockData.first_name} ${mockData.last_name}`}
-              email={mockData.email}
-            />
-          </div>
+        <Grid>  
+          {
+            users.map((user)=> 
+              <div className='flex-column items-center' key={user.id}>
+                <UserImage url={user.avatar} alt={user.first_name} />
+                <UserInfo
+                    fullName={`${user.first_name} ${user.last_name}`}
+                    email={user.email}
+                />
+            </div>
 
-          <div className='flex-column items-center'>
-            <UserImage url={mockData.avatar} alt={mockData.first_name} />
-            <UserInfo
-              fullName={`${mockData.first_name} ${mockData.last_name}`}
-              email={mockData.email}
-            />
-          </div>
-
-          <div className='flex-column items-center'>
-            <UserImage url={mockData.avatar} alt={mockData.first_name} />
-            <UserInfo
-              fullName={`${mockData.first_name} ${mockData.last_name}`}
-              email={mockData.email}
-            />
-          </div>
-
-          <div className='flex-column items-center'>
-            <UserImage url={mockData.avatar} alt={mockData.first_name} />
-            <UserInfo
-              fullName={`${mockData.first_name} ${mockData.last_name}`}
-              email={mockData.email}
-            />
-          </div>
+            )
+          }
         </Grid>
 
-        <div className='flex space-around mt-40'>
+        <div className="flex space-around mt-40">
           <Button
-            className='btn btn-sm'
-            btnType='primary'
-            type='button'
-            onClick={() => console.log('prev')}
+            className="btn btn-sm"
+            btnType="primary"
+            type="button"
+            onClick={ () => {
+              setCurrentPage(currentPage-1)
+              fetchUsers();
+            }
+          }
           >
             Prev users
           </Button>
 
           <Button
-            className='btn btn-sm'
-            btnType='primary'
-            type='button'
-            onClick={() => console.log('next')}
+            className="btn btn-sm"
+            btnType="primary"
+            type="button"
+            onClick={() => {
+              setCurrentPage(currentPage+1)
+              fetchUsers();
+              }
+            }
           >
             Next users
           </Button>
